a9a04812f6b52b7ef400aeb81cd9bfe39af3641b
MKeys-Capstone <mkeys@capstone-is.com>
2024-12-30 15:39:36 -0500

    install shared components as dep of monorepo

---
+++ libs/shared-components/src/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { IonButton } from '@ionic/react';
+
+const Button = ({ children, onClick }) => (
+  <IonButton onClick={onClick}>{children}</IonButton>
+);
+
+export default Button;
